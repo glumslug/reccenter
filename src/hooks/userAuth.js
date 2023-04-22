56c2481caa55facf120e694981e76befa81090b1
glumslug <glumaslug@gmail.com>
2023-04-22 01:18:56 -0400

    updated api url env

--- src/hooks/userAuth.js
+++ src/hooks/userAuth.js
@@ -3,63 +3,53 @@ import { useContext } from "react";
 import { toast } from "react-toastify";
 import { AuthContext } from "../context/AuthContext";
 
-const API_URL = '/api/users/'
-
+const API_URL = process.env.API_URL;
 
 // Register user
 const register = async (userData) => {
-    
-    const response = await axios.post(API_URL, userData)
+  const response = await axios.post(API_URL, userData);
 
-    if(response.data) {
-        localStorage.setItem('user', JSON.stringify(response.data))
-    }
+  if (response.data) {
+    localStorage.setItem("user", JSON.stringify(response.data));
+  }
 
-    return response.data
-}
+  return response.data;
+};
 
 // Login user
 const login = async (userData) => {
-    
-    
-    try {
-        const response = await axios.post(API_URL + 'login', userData)
-        if(response.data) {
-            localStorage.setItem('user', JSON.stringify(response.data))
-        }
-        
-        return response.data
-    } catch (error) {
-        
-        return error
-        
+  try {
+    const response = await axios.post(API_URL + "login", userData);
+    if (response.data) {
+      localStorage.setItem("user", JSON.stringify(response.data));
     }
-    
-}
+
+    return response.data;
+  } catch (error) {
+    return error;
+  }
+};
 
 // Refresh user
-const refresh = async (id) =>{
-    
-    
-    const response = await axios.get(API_URL + id)
+const refresh = async (id) => {
+  const response = await axios.get(API_URL + id);
 
-    if(response.data) {
-        
-        localStorage.setItem('user', JSON.stringify(response.data))
-        return response.data
-    }
-    
-    
-}
+  if (response.data) {
+    localStorage.setItem("user", JSON.stringify(response.data));
+    return response.data;
+  }
+};
 
 // Logout User
 const logout = () => {
-    localStorage.removeItem('user')
-    
-}
+  localStorage.removeItem("user");
+};
 
 const userAuth = {
-    register, logout, login, refresh
-}
+  register,
+  logout,
+  login,
+  refresh,
+};
 
-export default userAuth
+export default userAuth;
